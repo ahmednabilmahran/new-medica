089dca2a1acf659f81da113302067fc9ea8f4a59
Kareem Ayman <Kareem.ayman1622@gmail.com>
2022-07-12 09:26:35 +0200

    update on shop

--- lib/screens/catalog_screen.dart
+++ lib/screens/catalog_screen.dart
@@ -1,26 +1,267 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:medica/patient/patient_home.dart';
 import 'package:medica/screens/cart_screen.dart';
 import 'package:medica/view/widgets/catalog_products.dart';
+
+import '../patient/patient_profile.dart';
+import '../view/widgets/constance.dart';
+import '../view/widgets/custom_background.dart';
+import '../view/widgets/custom_text.dart';
 
 class CatalogScreen extends StatelessWidget {
   const CatalogScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Catalog")),
-      body: SafeArea(
-        child: Column(
+    final Size size = MediaQuery.of(context).size;
+    return WillPopScope(
+      onWillPop: () async {
+        // Get.to(loginAs());
+        Get.to(() => patient_home());
+        return true;
+      },
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
           children: [
-            CatalogProducts(),
-            ElevatedButton(
-              onPressed: () => Get.to(() => CartScreen()),
-              child: Text('Go to Cart'),
+            myDefaultBackground(),
+            Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/png/register.png'),
+                        fit: BoxFit.fill))),
+            Container(
+              // padding: EdgeInsets.symmetric(
+              //   horizontal: size.width * 0.05,
+              // ),
+              // color: Colors.green,
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.04),
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: size.height * 0.045,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(left: size.width * 0.04),
+                          child: Image.asset('assets/images/Menu.png')),
+                      CustomText(
+                        text: "USERNAME",
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.to(() => CartScreen());
+                        },
+                        child: SvgPicture.asset(
+                          'assets/icons/shopping-cart.svg',
+                          height: 25,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
+            Stack(alignment: Alignment.bottomCenter, children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                      // right: size.width * 0.08,
+                      // left: size.width * 0.08,
+                      top: size.height * 0.04,
+                    ),
+                    // color: Colors.green,
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(75),
+                        // topRight: Radius.circular(10),
+                      ),
+                      color: whitegrayish,
+                    ),
+                    height: size.height * 0.88,
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        Container(
+                          // padding: EdgeInsets.only(bot),
+                          margin: EdgeInsets.only(bottom: size.height * 0.02),
+                          child: Wrap(
+                            // alignment: WrapAlignment.start,
+                            // direction: Axis.horizontal,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(
+                                  right: size.width * 0.08,
+                                  left: size.width * 0.08,
+                                ),
+                                child: CustomText(
+                                  textAlign: TextAlign.left,
+                                  text: 'Popular products',
+                                  textStyle: TextStyle(
+                                    color: primaryColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Inter',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Scrollbar(
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              right: size.width * 0.08,
+                              left: size.width * 0.08,
+                            ),
+                            // width: double.infinity,
+                            // color: Colors.amber,
+                            height: size.height * 0.65,
+                            child: Column(
+                              // alignment: WrapAlignment.spaceBetween,
+                              // crossAxisAlignment: WrapCrossAlignment.end,
+                              children: [
+                                CatalogProducts(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                // padding: EdgeInsets.only(
+                //   top: size.height * 0.02,
+                //   right: size.width * 0.1,
+                //   left: size.width * 0.1,
+                // ),
+                width: double.infinity,
+                height: size.height * 0.080,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    )),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.04),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Get.to(() => patient_home());
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/images/homeNavBarHome.svg',
+                              color: primaryColorOutOfFocus,
+                            ),
+                            SizedBox(
+                              height: size.height * 0.008,
+                            ),
+                            CustomText(
+                              text: 'Home',
+                              textStyle:
+                                  TextStyle(color: primaryColorOutOfFocus),
+                            )
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                                'assets/images/clipboardNavBarHome.svg'),
+                            SizedBox(
+                              height: size.height * 0.008,
+                            ),
+                            CustomText(
+                              text: 'Consult',
+                              textStyle: TextStyle(color: linkColor),
+                            )
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                                'assets/images/message-circleNavBarHome.svg',
+                                color: primaryColorOutOfFocus),
+                            SizedBox(
+                              height: size.height * 0.008,
+                            ),
+                            CustomText(
+                              text: 'Chat',
+                              textStyle:
+                                  TextStyle(color: primaryColorOutOfFocus),
+                            )
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.to(() => patient_profile());
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset('assets/images/UserNavBarHome.svg',
+                                color: primaryColorOutOfFocus),
+                            SizedBox(
+                              height: size.height * 0.008,
+                            ),
+                            CustomText(
+                              text: 'Profile',
+                              textStyle:
+                                  TextStyle(color: primaryColorOutOfFocus),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ]),
           ],
         ),
       ),
     );
   }
 }
+// SafeArea(
+//         child: Column(
+//           children: [
+//             CatalogProducts(),
+//             ElevatedButton(
+//               onPressed: () => Get.to(() => CartScreen()),
+//               child: Text('Go to Cart'),
+//             ),
+//           ],
+//         ),
+//       ),
--- lib/view/widgets/catalog_products.dart
+++ lib/view/widgets/catalog_products.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:medica/controllers/cart_controller.dart';
 import 'package:medica/controllers/productController.dart';
-
+import 'package:medica/view/widgets/constance.dart';
+import 'package:medica/view/widgets/custom_text.dart';
 
 class CatalogProducts extends StatelessWidget {
   final productController = Get.put(ProductController());
@@ -11,9 +13,19 @@ class CatalogProducts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return Obx(
       () => Flexible(
-        child: ListView.builder(
+        child: GridView.builder(
+            // scrollDirection: ,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.6,
+              crossAxisSpacing: ((size.width + size.height) / 2) * 0.016,
+              mainAxisSpacing: ((size.width + size.height) / 2) * 0.016,
+            ),
+            scrollDirection: Axis.vertical,
             itemCount: productController.products.length,
             itemBuilder: (BuildContext context, int index) {
               return CatalogProductCard(index: index);
@@ -35,40 +47,110 @@ class CatalogProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 10,
+    final Size size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        color: Colors.white,
       ),
-      child: Row(
+      padding: EdgeInsets.symmetric(
+          // horizontal: 10,
+          // vertical: 20,
+          ),
+      height: size.height * 0.0,
+      // width: size.width * 0.38,
+      // margin: EdgeInsets.symmetric(horizontal: size.width * 0.02),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          CircleAvatar(
-            radius: 40,
-            backgroundImage: NetworkImage(
-              productController.products[index].imageUrl,
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Text(
-              productController.products[index].name,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: size.height * 0.008,
+                  right: size.width * 0.02,
+                  left: size.width * 0.02,
+                ),
+                height: size.height * 0.15,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      image: NetworkImage(
+                        productController.products[index].imageUrl,
+                      ),
+                    )),
               ),
-            ),
+              // CircleAvatar(
+              //   radius: 40,
+              //   backgroundImage: NetworkImage(
+              //     productController.products[index].imageUrl,
+              //   ),
+              // ),
+              SizedBox(height: size.height * 0.02),
+              Container(
+                child: Wrap(
+                  children: [
+                    Text(
+                      productController.products[index].name,
+                      style: TextStyle(
+                          color: primaryColor,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18,
+                          fontFamily: 'DMSans'),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Text(
+                  '\u1d31\u1d33\u1d3e${productController.products[index].price}',
+                  style: TextStyle(
+                    color: medicaCyan,
+                  ),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Text('${productController.products[index].price}'),
-          ),
-          IconButton(
-            onPressed: () {
-              cartController.addProduct(productController.products[index]);
-            },
-            icon: Icon(
-              Icons.add_circle,
-            ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  SvgPicture.asset('assets/icons/shopping-cart-right.svg'),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: size.width * 0.02,
+                    ),
+                    child: CustomText(
+                      text: 'ADD TO CART',
+                      textStyle: TextStyle(
+                          fontFamily: 'inter',
+                          color: Colors.white,
+                          fontSize: 12),
+                    ),
+                  )
+                ]),
+                style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    )),
+                    backgroundColor: Color(0xff603EBC),
+                    // primary: Colors.transparent,
+                    minimumSize: Size(double.infinity, size.height * .07)),
+                onPressed: () {
+                  cartController.addProduct(productController.products[index]);
+                },
+              ),
+            ],
           ),
         ],
       ),
